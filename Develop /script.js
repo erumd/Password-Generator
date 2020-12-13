1b3a5dda3ffa6b7262e54d034cf3b48292c96a7e
Erum Dhukka <erumdhukka531@gmail.com>
2020-12-13 16:14:32 -0600

    added working functions

--- Develop /script.js
+++ Develop /script.js
@@ -7,61 +7,116 @@
 //   alert (passLength);
 // }
 
+var generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+
+
 // 2nd way 
 document.querySelector('#generate').addEventListener('click', promptMe);
 function promptMe() {
     var passLength = prompt("How long do you want your password to be? Pick between 8- 128 characters");
-    var passLowerCase = prompt('Do you want lower case in your password? Type "yes" or "no"');
-    var passUpperCase = prompt('Do you want UPPER case in your password? Type "yes" or "no" ');
-    var passNumber = prompt('Do you want numbers in your password? Type "yes" or "no"');
-    var passCharacters = prompt('Do you want special characters in your password? Type "yes" or "no"');
-    alert (passLength, passLowerCase);
+    var passLowerCase = confirm ('Do you want "lower case" in your password? Press OK to confirm and cancel to not include.');
+    var passUpperCase = confirm ('Do you want "UPPER case" in your password? Press OK to confirm and cancel to not include. ');
+    var passNumber = confirm ('Do you want "numbers" in your password? Press OK to confirm and cancel to not include.');
+    var passCharacters = confirm ('Do you want "special characters" in your password? Press OK to confirm and cancel to not include.');
+    // writePassword ( "passLength, passLowerCase, passUpperCase, passNumber, passCharacters;
 }
-
-
 console.log (promptMe);
 
-// tutor help
-// document.getElementById("generate").addEventListener("click", function(
-//   prompt= "Choose a length of at least 8 characters and no more than 128 characters" )
-
-// prompt1
-// click= prompt ("Choose a length of at least 8 characters and no more than 128 characters");
-// document.addEventListener("click", function(
-//   click= prompt ("Choose a length of at least 8 characters and no more than 128 characters")
-// ){
-
-
-
-
-var randomLower
-var randomUpper
-var randomNumber
-var randomSymbol
-
-
-
-
-
-
-
-
+// restricting less than 8 
+// if (passCharacters) {
+//   =parseInt (prompt "Enter length")
+// } else {
+// }
+// loop to take user input and generate password 
+// include letters can NOT be included in length 
 // Assignment Code
-var generateBtn = document.querySelector("#generate")
+// var generateBtn = document.querySelector("#generate")
+passLowerCase= 'abcdefghijklmnopqrstuvwxyz';
+passUpperCase= 'ABCDEFGHIJKLMNOPQRSTUVWZYZ';
+passNumber= '0123456789'
+passCharacters=  " !'#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 
 // Write password to the #password input
 function writePassword( passLength, passLowerCase, passUpperCase, passNumber, passCharacters) {
   // var password = generatePassword();
+  console.log ('writePassword');
+}
+
+  // password values 
+  // let values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+  // let password= "";
+
+  // loop to generate password characters
+  for (let i = 0; i < passCharacters.length; i++) {
+    console.log[i];
+    
+  }
+
+  // function to generate password in array 
+  
+  function randomLower () {
+    passLowerCase= 'abcdefghijklmnopqrstuvwxyz';
+    var randomPassword= "";
+    var randomGenerate = Math.floor(Math.random() * passLowerCase.length);
+    passLowerCase.substring(randomGenerate, +1);
+    console.log (randomPassword);
+    return randomPassword;
+    console.log (randomPassword);
+  }
+  
+
+  function randomUpper () {
+    passLowerCase= 'ABCDEFGHIJKLMNOPQRSTUVWZYZ';
+    var randomPassword;
+    var randomGenerate = Math.floor(Math.random() * passLowerCase.length);
+    passUpperCase.substring(randomPassword, +1);
+    console.log (randomPassword);
+    return randomPassword;
+    console.log (randomPassword);
+  }
+
+
+  function randomNumber () {
+    passNumber= '0123456789';
+    var randomPassword;
+    var randomGenerate = Math.floor(Math.random() * passNumber.length);
+    passNumber.substring(randomPassword, +1);
+    console.log (randomPassword);
+    return randomPassword;
+    console.log (randomPassword);
+  // }
+
+  function randomNumber () {
+    passCharacters= " !'#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    var randomPassword;
+    var randomGenerate = Math.floor(Math.random() * passCharacters.length);
+    passCharacters.substring(randomPassword, +1);
+    console.log (randomPassword);
+    return randomPassword;
+    console.log (randomPassword);
+  }
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-  const typesCount = passLength +passLowerCase +passUpperCase + passNumber +passCharacters;
+  // const typesCount = passLength +passLowerCase +passUpperCase + passNumber +passCharacters;
+  // console.log ('typesCount');
 
-  const typesArray = [passLength, passLowerCase, passUpperCase, passNumber, passCharacters];
+  // const typesArray = [passLength, passLowerCase, passUpperCase, passNumber, passCharacters];
+  // console.log ('typesArray');
 
+function finalRandomPassword(params) {
+  return randomPassword
+}
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+generateBtn.addEventListener("click", writePassword());
+
+
+// result.innerText = generateBtn ()
