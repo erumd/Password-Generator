fb0c975cfee5522061c9c2b9a4ae5ff4abcbe097
Erum Dhukka <erumdhukka531@gmail.com>
2020-12-13 09:15:24 -0600

    promts working. trying to generate pass

--- Develop /script.js
+++ Develop /script.js
@@ -9,33 +9,15 @@
 
 // 2nd way 
 document.querySelector('#generate').addEventListener('click', promptMe);
-
 function promptMe() {
     var passLength = prompt("How long do you want your password to be? Pick between 8- 128 characters");
-    alert (passLength);
+    var passLowerCase = prompt('Do you want lower case in your password? Type "yes" or "no"');
+    var passUpperCase = prompt('Do you want UPPER case in your password? Type "yes" or "no" ');
+    var passNumber = prompt('Do you want numbers in your password? Type "yes" or "no"');
+    var passCharacters = prompt('Do you want special characters in your password? Type "yes" or "no"');
+    alert (passLength, passLowerCase);
 }
 
-// prompt2 Character types to include in the password
-// Choose lowercase, uppercase, numeric, and/or special characters
-function promptMe() {
-    var passLowerCase = prompt("Do you want lower case in your password?");
-    alert (passLength);
-}
-
-function promptMe() {
-  var passUpperCase = prompt("Do you want UPPER case in your password?");
-  alert (passLength);
-}
-
-function promptMe() {
-  var passUpperCase = prompt("Do you want numbers in your password?");
-  alert (passLength);
-}
-
-function promptMe() {
-  var passUpperCase = prompt("Do you want special characters in your password?");
-  alert (passLength);
-}
 
 console.log (promptMe);
 
@@ -69,11 +51,15 @@ var generateBtn = document.querySelector("#generate")
 
 
 // Write password to the #password input
-function writePassword() {
+function writePassword( passLength, passLowerCase, passUpperCase, passNumber, passCharacters) {
   // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  const typesCount = passLength +passLowerCase +passUpperCase + passNumber +passCharacters;
+
+  const typesArray = [passLength, passLowerCase, passUpperCase, passNumber, passCharacters];
 
 }
 
