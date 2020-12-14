b4d841edbece19a62b4faa105faa0dd18b3655d8
Erum Dhukka <erumdhukka531@gmail.com>
2020-12-13 22:43:12 -0600

    function condense. prompt gone :(

--- Develop /script.js
+++ Develop /script.js
@@ -22,11 +22,12 @@ function generatePassword() {
       // if else statement needs to go after passLength or will not work for within 8-128. 
       if (!Number.isInteger(passLength)) {
         alert ("You have to enter a number");
-        // need return promtMe so user does not have to go through the confirm messages
+        // need return to main prompt so user does not have to go through the confirm messages
         return generatePassword();
       }
       if (passLength <8 || passLength >128 ) {
         alert ("Pick password length from 8-128 characters") 
+        // need return to main prompt so user does not have to go through the confirm messages
         return promptMe();
       } else {
       var passLowerCase = confirm ('Do you want "lower case" in your password? Press OK to confirm and cancel to not include.');
@@ -55,26 +56,45 @@ console.log (generatePassword);
       
     // }
 
-    // function to generate password in array 
-    
+    // function to general char. NOT SURE???????????????????
+    function general(chars) {
+      var randomGenerate = Math.floor(Math.random() * chars.length);
+          chars.substring(randomGenerate, +1);
+          console.log (randomPassword);
+          return randomPassword;
+     }
+
+
+    // function randomLower () {
+    //   passLowerCase= 'abcdefghijklmnopqrstuvwxyz';
+    //   var randomPassword= '';
+    //   var randomGenerate = Math.floor(Math.random() * passLowerCase.length);
+    //   passLowerCase.substring(randomGenerate, +1);
+    //   console.log (randomPassword);
+    //   return randomPassword;
+    //   console.log (randomPassword);
+    // }
+    // tutor
     function randomLower () {
       passLowerCase= 'abcdefghijklmnopqrstuvwxyz';
-      var randomPassword= '';
-      var randomGenerate = Math.floor(Math.random() * passLowerCase.length);
-      passLowerCase.substring(randomGenerate, +1);
-      console.log (randomPassword);
-      return randomPassword;
+      var randomPassword = general(passLowerCase);
       console.log (randomPassword);
     }
-    
 
-    function randomUpper () {
-      passLowerCase= 'ABCDEFGHIJKLMNOPQRSTUVWZYZ';
-      var randomPassword; "";
-      var randomGenerate = Math.floor(Math.random() * passLowerCase.length);
-      passUpperCase.substring(randomPassword, +1);
-      console.log (randomPassword);
-      return randomPassword;
+    // function randomUpper () {
+    //   passLowerCase= 'ABCDEFGHIJKLMNOPQRSTUVWZYZ';
+    //   var randomPassword; "";
+    //   var randomGenerate = Math.floor(Math.random() * passLowerCase.length);
+    //   passUpperCase.substring(randomPassword, +1);
+    //   console.log (randomPassword);
+    //   return randomPassword;
+    //   console.log (randomPassword);
+    // }
+
+    // tutor
+    function randomUpper() {
+      passUpperCase= 'ABCDEFGHIJKLMNOPQRSTUVWZYZ';
+      var randomPassword = general (passUpperCase);
       console.log (randomPassword);
     }
 
